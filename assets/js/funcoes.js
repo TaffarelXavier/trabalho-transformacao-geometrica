26631c09a12fcfb69a89c2a72dcdb13d076cb094
TaffarelXavier <taffarel_deus@hotmail.com>
2019-10-07 13:24:42 -0300

    Alterações

--- assets/js/funcoes.js
+++ assets/js/funcoes.js
@@ -1,23 +1,29 @@
+let btnNotacao = document.getElementById("rotacao");
+let btnTranslacao = document.getElementById("translacao");
+let btnCisalhamento = document.getElementById("cisalhamento");
+
 /**
  * Configurações Iniciais do Chats
  */
 var obj = {
-	animationEnabled: true,
-	zoomEnabled: true,
-	title:{
-		text: "Plano"
-	},
-	axisX: {
-		title:"X"
-	},
-	axisY:{
-		title: "Y",
-	},
-	data: [{
-		type: "scatter",
-		toolTipContent: "<b>X </b>{x}<br/><b>Y </b>${y}k",
-		dataPoints: []
-	}]
+  animationEnabled: true,
+  zoomEnabled: true,
+  title: {
+    text: "Exemplicação usando um Plugin para mostrar no Plano",
+  },
+  axisX: {
+    title: "X",
+  },
+  axisY: {
+    title: "Y",
+  },
+  data: [
+    {
+      type: "scatter",
+      toolTipContent: "<b>X:</b>{x}<br/><b>Y:</b>{y}",
+      dataPoints: [],
+    },
+  ],
 };
 
 //Criei esta matriz para armazenar os pontos, quando se clica no plano
@@ -39,9 +45,9 @@ function cnvs_clearCoordinates() {
   document.title = "Matrizes de Transformação Geométrica";
 }
 
-/**
- *
- * */
+//Pega uma matriz de ponto, por exemplo: x e y
+//e multiplica por uma outra matriz e retorna uma nova
+//coordenada
 function projecaoDePonto(ponto, _matriz) {
   let x = Math.round(ponto[0] * _matriz[0][0] + ponto[1] * _matriz[0][1]);
 
@@ -56,13 +62,13 @@ function projecaoDePonto(ponto, _matriz) {
         **/
 function rotacao(ponto, angulo) {
   //A conversão é necessária,
-  //pois as funções cos e sin, de javascript
-  //retorna em radianos
+  //pois as funções cos e sin de javascript
+  //são retornadas em radianos
   let alfa = (angulo * Math.PI) / 180;
 
   let matrizRotacao = [
     [Math.cos(alfa), -Math.sin(alfa)],
-    [Math.sin(alfa), Math.cos(alfa)]
+    [Math.sin(alfa), Math.cos(alfa)],
   ];
 
   return projecaoDePonto(ponto, matrizRotacao);
@@ -136,13 +142,11 @@ document.getElementById("coordiv").onclick = function(ev) {
   //$('#get-pontos').append("PONTO: ").append(`${letraPonto}[${x},${y}]<br/>`);
 };
 
-
-
 let buttonCalcular = document.getElementById("calcular");
 buttonCalcular.onclick = function() {
   for (let { x, y } of matriz) {
-    let  coord = escalamento([x, y],5,5);
-console.log(coord);
+    let coord = escalamento([x, y], 5, 5);
+    console.log(coord);
     obj.data[0].dataPoints.push({ x: coord.x, y: coord.y });
   }
 };
@@ -178,4 +182,56 @@ document.getElementById("reduzir-aumentar").onmouseup = function() {
   iniciar = true;
   var chart = new CanvasJS.Chart("chartContainer", obj);
   chart.render();
+};
+
+//Para rotação:
+btnNotacao.oninput = function() {
+  let angulo = this.value;
+  console.log(angulo);
+  for (let { x, y } of matriz) {
+    let coordenadas = rotacao([x, y], angulo);
+    obj.data[0].dataPoints.push({ x: coordenadas.x, y: coordenadas.y });
+  }
+};
+
+btnNotacao.onmouseup = function() {
+  setTimeout(function() {
+    var chart = new CanvasJS.Chart("chartContainer", obj);
+    chart.render();
+  }, 1000); //Espera-se um atraso de 1 segundo(1000 milésimo).
+};
+
+//Para translação:
+btnTranslacao.onmouseup = function() {
+  let angulo = this.value;
+  for (let { x, y } of matriz) {
+    console.log(x, y);
+    let coordenadas = translacao([x, y], [parseInt(angulo), parseInt(angulo)]);
+    obj.data[0].dataPoints.push({ x: coordenadas.x, y: coordenadas.y });
+  }
+
+  setTimeout(function() {
+    var chart = new CanvasJS.Chart("chartContainer", obj);
+    chart.render();
+  }, 1000); //Espera-se um atraso de 1 segundo(1000 milésimo).
+};
+
+//btnCisalhamento: 
+btnCisalhamento.onmouseup = function() {
+  let valor = this.value;
+
+
+  for (let { x, y } of matriz) {
+    
+    let coordenadas = cisalhamento([x, y], parseInt(valor));
+    obj.data[0].dataPoints.push({ x: coordenadas.x, y: coordenadas.y });
+  }
+
+
+  console.log( obj.data[0].dataPoints);
+  /*setTimeout(function() {
+    var chart = new CanvasJS.Chart("chartContainer", obj);
+    chart.render();
+  }, 1000); //Espera-se um atraso de 1 segundo(1000 milésimo).
+*/
 };
